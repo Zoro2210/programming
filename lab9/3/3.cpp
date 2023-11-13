1fa4871a73c77a67185fb30852e70b67b0944c3d
Dima <gulkadima49@gmail.com>
2023-11-13 14:44:33 +0200

    re-release lab9-3

--- lab9/3/3.cpp
+++ lab9/3/3.cpp
@@ -1,20 +1,39 @@
-#include<iostream>
-
+```
+#include <iostream>
 using namespace std;
 
-int PrintFactorial(int n) {
-	if (n == 0) {
-		return 1;
-	}
-
-	return n * (n - 1);
+unsigned long long factorial(int n) {
+    if (n == 0  n == 1) {  // Fixed the condition here
+        return 1;
+    }
+    else {
+        unsigned long long result = 1;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
 }
 
 int main() {
-	int n;
-	cout << "Specify pure to calculate the factorial: ";
-	cin >> n;
-	int PF = PrintFactorial(n);
-	cout << "Your numer " << n << " is equal to factorial "  << PF << endl;
+    int n;
+    char choice;
 
+    do {
+        cout << "Enter an integer to calculate its factorial: ";
+        cin >> n;
+
+        if (n < 0) {
+            cout << "Factorial is defined only for non-negative integers." << endl;
+        }
+        else {
+            cout << n << "! = " << factorial(n) << endl;
+        }
+
+        cout << "Do you want to continue (y/n)? ";
+        cin >> choice;
+    } while (choice == 'y'  choice == 'Y');  // Fixed the condition here
+
+    return 0;
 }
+```
